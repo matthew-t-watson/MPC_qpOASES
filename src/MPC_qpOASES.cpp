9edbf7be83e2d747f0746e916928739759066231
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-08 10:50:25 +0000

    Ditched the use of matlab generated functions

--- src/MPC_qpOASES.cpp
+++ src/MPC_qpOASES.cpp
@@ -1,5 +1,6 @@
 
 #include <qpOASES.hpp>
+#include "Eigen"
 
 int main()
 {
@@ -7,6 +8,12 @@ int main()
 }
 
 int computeMPC(const double *x, const double *r, double *b, double *c,
+        double *u)
+{
+
+}
+
+void calculate_u(const double *x, const double *r, const double *Pr, const double *c,
         double *u)
 {
 
