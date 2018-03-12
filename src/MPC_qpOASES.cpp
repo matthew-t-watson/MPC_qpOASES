7ad6915c1a932f8a4ae9059df7c125d55bca8906
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-12 10:49:26 +0000

    .

--- src/MPC_qpOASES.cpp
+++ src/MPC_qpOASES.cpp
@@ -21,8 +21,9 @@ int main()
 	/* Test routine */
 	QP_res_t QP_res;
 	double u[NU] = {0};
+	double x={0,1,0,0,0,0,0,0};
 
-	int exitFlag = computeMPC(QP, x0, r0, &QP_res, u);
+	int exitFlag = computeMPC(QP, x, r0, &QP_res, u);
 }
 
 int initMPC(qpOASES::QProblem& QP)
