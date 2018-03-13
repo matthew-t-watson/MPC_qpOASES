115578c7db492ecc2b6f74f3f0a7c3828a38950e
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-13 08:17:10 +0000

    .

--- include/MPC_qpOASES/networking.hpp
+++ include/MPC_qpOASES/networking.hpp
@@ -2,7 +2,22 @@
 #ifndef NETWORKING_H
 #define NETWORKING_H
 
+#include "MPC_defines.hpp"
+
+typedef struct MPCPacketParams_t
+{
+	double x[NX];
+	double r[NX*NR];
+};
+
+typedef struct MPCPacketResult_t
+{
+	double x[NX];
+	double r[NX*NR];
+};
+
 int configureSockets();
 int getPacket();
+int getInterfaceIP(char* ip, int len, const char* interface)
 
 #endif
--- src/networking.cpp
+++ src/networking.cpp
@@ -37,7 +37,9 @@ int configureSockets()
 	si_odroid.sin_addr.s_addr = htonl(INADDR_ANY);
 	inet_pton(AF_INET, odroidAddr, &(si_odroid.sin_addr));
 
-	//bind socket to port
+	getInterfaceIP(NULL,0,NULL);
+
+	/* bind socket to port */
 	if (bind(s, (struct sockaddr*) &si_odroid, sizeof(si_odroid)) == -1)
 	{
 		printf("Failed to bind socket to port, errno %i\n", errno);
@@ -72,4 +74,64 @@ int sendPacket()
 //		printf("Error in sendto, errno %i\n", errno);
 //		return errno;
 //	}
+}
+
+int getInterfaceIP(char* ip, int len, const char* interface)
+{
+	struct ifaddrs *ifaddr, *ifa;
+	int family, s, n;
+	char host[NI_MAXHOST];
+
+	if (getifaddrs(&ifaddr) == -1)
+	{
+		printf("Error in getifaddrs, errno %i\n", errno);
+		return errno;
+	}
+
+	for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++)
+	{
+		if (ifa->ifa_addr == NULL)
+			continue;
+
+		family = ifa->ifa_addr->sa_family;
+
+		/* Display interface name and family (including symbolic
+		 form of the latter for the common families) */
+
+		printf("%-8s %s (%d)\n", ifa->ifa_name,
+				(family == AF_PACKET) ? "AF_PACKET" :
+				(family == AF_INET) ? "AF_INET" :
+				(family == AF_INET6) ? "AF_INET6" : "???", family);
+
+		/* For an AF_INET* interface address, display the address */
+
+		if (family == AF_INET || family == AF_INET6)
+		{
+			s = getnameinfo(ifa->ifa_addr,
+					(family == AF_INET) ?
+							sizeof(struct sockaddr_in) :
+							sizeof(struct sockaddr_in6), host, NI_MAXHOST, NULL,
+					0, NI_NUMERICHOST);
+			if (s != 0)
+			{
+				printf("getnameinfo() failed: %s\n", gai_strerror(s));
+				exit (EXIT_FAILURE);
+			}
+
+			printf("\t\taddress: <%s>\n", host);
+
+		}
+		else if (family == AF_PACKET && ifa->ifa_data != NULL)
+		{
+			struct rtnl_link_stats *stats = ifa->ifa_data;
+
+			printf("\t\ttx_packets = %10u; rx_packets = %10u\n"
+					"\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
+					stats->tx_packets, stats->rx_packets, stats->tx_bytes,
+					stats->rx_bytes);
+		}
+	}
+
+	freeifaddrs(ifaddr);
+	exit (EXIT_SUCCESS);
 }
