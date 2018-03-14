ab1af6bb3aa362cf76b93a9867fad27eed56a53e
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-14 15:54:51 +0000

    .

--- src/networking.cpp
+++ src/networking.cpp
@@ -89,12 +89,6 @@ int sendPacket(MPCPacketResult_t& data)
 	printMatrix("s", data.s, 1, NS);
 	printMatrix("u", data.u, 1, NU);
 
-	/* Swap double words back */
-//	swapDoubleWords(data.c, data.c_len);
-//	swapDoubleWords(data.cinf, data.cinf_len);
-//	swapDoubleWords(data.s, data.s_len);
-//	swapDoubleWords(data.u, data.u_len);
-
 	/* Send packet */
 	if (sendto(s, &data, sizeof(data), 0, (struct sockaddr*) &si_myrio, slen) == -1)
 	{
