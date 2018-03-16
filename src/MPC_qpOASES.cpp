74a41ce015894fee0e9272fc28772abf5fb4616e
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-16 17:50:23 +0000

    Reverted attempts at using cinf guess to initialise

--- src/MPC_qpOASES.cpp
+++ src/MPC_qpOASES.cpp
@@ -6,33 +6,31 @@
 #include "MPC_int_consts.hpp"
 #include "calculate_u.h"
 #include "calculate_b.h"
-#include "calculate_cinf.h"
 #include "networking.hpp"
 #include <errno.h>
 #include "string.h" /* for memcpy */
 
 
-int computeMPC(qpOASES::QProblem& QP, qpOASES::SymSparseMat* Hsp, qpOASES::SparseMatrix* Asp, MPCPacketParams_t& params, MPCPacketResult_t& res);
+int initMPC(qpOASES::SQProblemSchur& QP);
+int initMPC(qpOASES::QProblem& QP);
+int computeMPC(qpOASES::SQProblemSchur& QP, MPCPacketParams_t& params, MPCPacketResult_t& res);
+int computeMPC(qpOASES::QProblem& QP, MPCPacketParams_t& params, MPCPacketResult_t& res);
 
 
 static const double G[NC*NU+NU+NS] = {0};
-//static const double x0[] = {0,0,0,0,0,0,0,0};
-//static const double r0[NR*NX] = {0};
+static const double x0[] = {0,0,0,0,0,0,0,0};
+static const double r0[NR*NX] = {0};
 
 #define USE_SPARSE_MATRICES
-#define INITIALISED_HOMOTOPY
 
 int main()
 {
 	/* Allocate QProblem object */
 	qpOASES::QProblem QP(NC*NU+NU+NS,NCON,qpOASES::HST_POSDEF);
+	//qpOASES::SQProblemSchur QP(NC*NU+NU+NS,NCON,qpOASES::HST_POSDEF);
 
 	/* Initialise QP */
-	qpOASES::SymSparseMat *Hsp = new qpOASES::SymSparseMat(H_NROWS, H_NCOLS, H_NCOLS, H);
-	qpOASES::SparseMatrix *Asp = new qpOASES::SymSparseMat(A_NROWS, A_NCOLS, A_NCOLS, A);
-
-	Hsp->createDiagInfo();
-	Asp->createDiagInfo();
+	initMPC(QP);
 
 	/* Configure UDP socket */
 	if (configureSockets() == 0)
@@ -46,7 +44,7 @@ int main()
 				continue;
 			}
 
-			computeMPC(QP, Hsp, Asp, MPCParams, MPCRes);
+			computeMPC(QP, MPCParams, MPCRes);
 
 			if (sendPacket(MPCRes) > 0)
 			{
@@ -62,13 +60,11 @@ int main()
 }
 
 
-int computeMPC(qpOASES::QProblem& QP, qpOASES::SymSparseMat* Hsp, qpOASES::SparseMatrix* Asp, MPCPacketParams_t& params, MPCPacketResult_t& res)
-{
-	static QPout_t QPout;
 
-	/* Calculate cinf */
-	calculate_cinf(params.x, params.r, QPout.cinf);
-	printf("cinf = [%f\t%f\t%f\t%f]\n", QPout.cinf[0], QPout.cinf[1], QPout.cinf[2], QPout.cinf[3]);
+int initMPC(qpOASES::QProblem& QP)
+{
+	int nWSR = 10000;
+	double cpuTime = 10;
 
 	qpOASES::Options opt;
 	opt.setToMPC(); /*  Sets all options to values resulting in minimum solution time */
@@ -76,17 +72,43 @@ int computeMPC(qpOASES::QProblem& QP, qpOASES::SymSparseMat* Hsp, qpOASES::Spars
 	opt.enableEqualities = qpOASES::BT_FALSE;
 	opt.enableInertiaCorrection = qpOASES::BT_FALSE;
 
+	opt.print();
 	QP.setOptions(opt);
 
 	/* Calculate b */
 	double b[NCON];
+	calculate_b(x0, r0, b);
+
+#ifdef USE_SPARSE_MATRICES
+	/* create sparse matrices */
+	qpOASES::SymSparseMat *Hsp = new qpOASES::SymSparseMat(H_NROWS, H_NCOLS, H_NCOLS, H);
+	qpOASES::SparseMatrix *Asp = new qpOASES::SymSparseMat(A_NROWS, A_NCOLS, A_NCOLS, A);
+
+	/* Init QP - sparse */
+	int exitFlag = QP.init(Hsp, G, Asp, NULL, NULL, NULL, b, nWSR, &cpuTime);
+#else
+	/* Init QP - dense */
+	int exitFlag = QP.init(H, G, A, NULL, NULL, NULL, b, nWSR, &cpuTime);
+#endif
+
+	printf("exitFlag %i, cpu time %fs, nWSR = %i\n", exitFlag, cpuTime, nWSR);
+
+	return exitFlag;
+}
+
+int computeMPC(qpOASES::QProblem& QP, MPCPacketParams_t& params, MPCPacketResult_t& res)
+{
+	/* Calculate b */
+	double b[NCON];
 	calculate_b(params.x, params.r, b);
 
-	res.nWSR = 100000;
+	/* Compute hotstarted QP */
+	res.nWSR = 1000;
 	res.tExec = 10;
-	res.exitFlag = QP.init(Hsp, G, Asp, NULL, NULL, NULL, b, res.nWSR, &res.tExec, QPout.z);
+	res.exitFlag = QP.hotstart(G, NULL, NULL, NULL, b, res.nWSR, &res.tExec);
 
 	/* Get result */
+	QPout_t QPout;
 	QP.getPrimalSolution( QPout.z );
 	res.cost = QP.getObjVal();
 
