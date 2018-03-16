92d3aa238ffd6b770cdc9b85cd18c762ba5825ff
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-16 12:09:28 +0000

    Added overloaded initMPC and computeMPC functions for SQProblemSchur

--- src/MPC_qpOASES.cpp
+++ src/MPC_qpOASES.cpp
@@ -11,8 +11,11 @@
 #include "string.h" /* for memcpy */
 
 
+int initMPC(qpOASES::SQProblemSchur& QP);
 int initMPC(qpOASES::QProblem& QP);
+int computeMPC(qpOASES::SQProblemSchur& QP, MPCPacketParams_t& params, MPCPacketResult_t& res);
 int computeMPC(qpOASES::QProblem& QP, MPCPacketParams_t& params, MPCPacketResult_t& res);
+
 
 static const double G[NC*NU+NU+NS] = {0};
 static const double x0[] = {0,0,0,0,0,0,0,0};
@@ -23,7 +26,8 @@ static const double r0[NR*NX] = {0};
 int main()
 {
 	/* Allocate QProblem object */
-	qpOASES::QProblem QP(NC*NU+NU+NS,NCON,qpOASES::HST_POSDEF);
+	//qpOASES::QProblem QP(NC*NU+NU+NS,NCON,qpOASES::HST_POSDEF);
+	qpOASES::SQProblemSchur QP(NC*NU+NU+NS,NCON,qpOASES::HST_POSDEF);
 
 	/* Initialise QP */
 	initMPC(QP);
@@ -53,6 +57,42 @@ int main()
 		printf("Failed to configure socket\n");
 		return 1;
 	}
+}
+
+
+int initMPC(qpOASES::SQProblemSchur& QP)
+{
+	int nWSR = 10000;
+	double cpuTime = 10;
+
+	qpOASES::Options opt;
+	opt.setToMPC(); /*  Sets all options to values resulting in minimum solution time */
+	opt.printLevel = qpOASES::PL_LOW;
+	opt.enableEqualities = qpOASES::BT_FALSE;
+	opt.enableInertiaCorrection = qpOASES::BT_FALSE;
+
+	opt.print();
+	QP.setOptions(opt);
+
+	/* Calculate b */
+	double b[NCON];
+	calculate_b(x0, r0, b);
+
+#ifdef USE_SPARSE_MATRICES
+	/* create sparse matrices */
+	qpOASES::SymSparseMat *Hsp = new qpOASES::SymSparseMat(H_NROWS, H_NCOLS, H_NCOLS, H);
+	qpOASES::SparseMatrix *Asp = new qpOASES::SymSparseMat(A_NROWS, A_NCOLS, A_NCOLS, A);
+
+	/* Init QP - sparse */
+	int exitFlag = QP.init(Hsp, G, Asp, NULL, NULL, NULL, b, nWSR, &cpuTime);
+#else
+	/* Init QP - dense */
+	int exitFlag = QP.init(H, G, A, NULL, NULL, NULL, b, nWSR, &cpuTime);
+#endif
+
+	printf("exitFlag %i, cpu time %fs, nWSR = %i\n", exitFlag, cpuTime, nWSR);
+
+	return exitFlag;
 }
 
 int initMPC(qpOASES::QProblem& QP)
@@ -88,6 +128,46 @@ int initMPC(qpOASES::QProblem& QP)
 	printf("exitFlag %i, cpu time %fs, nWSR = %i\n", exitFlag, cpuTime, nWSR);
 
 	return exitFlag;
+}
+
+int computeMPC(qpOASES::SQProblemSchur& QP, MPCPacketParams_t& params, MPCPacketResult_t& res)
+{
+	/* Calculate b */
+	double b[NCON];
+	calculate_b(params.x, params.r, b);
+
+	/* Compute hotstarted QP */
+	res.nWSR = 1000;
+	res.tExec = 10;
+	res.exitFlag = QP.hotstart(G, NULL, NULL, NULL, b, res.nWSR, &res.tExec);
+
+	/* Get result */
+	QPout_t QPout;
+	QP.getPrimalSolution( QPout.z );
+	res.cost = QP.getObjVal();
+
+	/* Copy remaining QP results into packet */
+	res.id = params.id;
+	memcpy(&res.c, &QPout.c, sizeof(res.c));
+	memcpy(&res.cinf, &QPout.cinf, sizeof(res.cinf));
+	memcpy(&res.s, &QPout.s, sizeof(res.s));
+
+	/* Calculate u */
+	calculate_u(params.x, params.r, res.c, res.u);
+
+	if (res.exitFlag != 0)
+	{
+		for (int i = 0; i < sizeof(res.u)/sizeof(res.u[0]); i++)
+		{
+			res.u[i] = 0;
+		}
+	}
+
+#ifdef VERBOSE
+	printf("exitFlag %i, cpu time %fs, nWSR = %i\n", res.exitFlag, res.tExec, res.nWSR);
+#endif
+
+	return res.exitFlag;
 }
 
 int computeMPC(qpOASES::QProblem& QP, MPCPacketParams_t& params, MPCPacketResult_t& res)
