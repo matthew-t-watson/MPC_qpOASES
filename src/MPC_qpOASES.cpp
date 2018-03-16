da22fbd12b907bdd3110870c2b3fd9d44abc612e
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-16 15:57:03 +0000

    First attempt at initialised homotopy

--- src/MPC_qpOASES.cpp
+++ src/MPC_qpOASES.cpp
@@ -11,9 +11,7 @@
 #include "string.h" /* for memcpy */
 
 
-int initMPC(qpOASES::SQProblemSchur& QP);
-int initMPC(qpOASES::QProblem& QP);
-int computeMPC(qpOASES::SQProblemSchur& QP, MPCPacketParams_t& params, MPCPacketResult_t& res);
+int initMPC(qpOASES::QProblem& QP, qpOASES::SymSparseMat *Hsp, qpOASES::SparseMatrix *Asp);
 int computeMPC(qpOASES::QProblem& QP, MPCPacketParams_t& params, MPCPacketResult_t& res);
 
 
@@ -22,6 +20,7 @@ static const double x0[] = {0,0,0,0,0,0,0,0};
 static const double r0[NR*NX] = {0};
 
 #define USE_SPARSE_MATRICES
+#define INITIALISED_HOMOTOPY
 
 int main()
 {
@@ -30,7 +29,9 @@ int main()
 	//qpOASES::SQProblemSchur QP(NC*NU+NU+NS,NCON,qpOASES::HST_POSDEF);
 
 	/* Initialise QP */
-	initMPC(QP);
+	qpOASES::SymSparseMat *Hsp = new qpOASES::SymSparseMat(H_NROWS, H_NCOLS, H_NCOLS, H);
+	qpOASES::SparseMatrix *Asp = new qpOASES::SymSparseMat(A_NROWS, A_NCOLS, A_NCOLS, A);
+	initMPC(QP, Hsp, Asp);
 
 	/* Configure UDP socket */
 	if (configureSockets() == 0)
@@ -61,10 +62,15 @@ int main()
 
 
 
-int initMPC(qpOASES::QProblem& QP)
+
+
+
+	/* Init QP - sparse */
+	int exitFlag = QP.init(Hsp, G, Asp, NULL, NULL, NULL, b, nWSR, &cpuTime);
+
+int computeMPC(qpOASES::QProblem& QP, qpOASES::SymSparseMat *Hsp, qpOASES::SparseMatrix *Asp, MPCPacketParams_t& params, MPCPacketResult_t& res)
 {
-	int nWSR = 10000;
-	double cpuTime = 10;
+	static double initialPrimalSolution[NC*NU+NU+NS] = {0};
 
 	qpOASES::Options opt;
 	opt.setToMPC(); /*  Sets all options to values resulting in minimum solution time */
@@ -72,40 +78,15 @@ int initMPC(qpOASES::QProblem& QP)
 	opt.enableEqualities = qpOASES::BT_FALSE;
 	opt.enableInertiaCorrection = qpOASES::BT_FALSE;
 
-	opt.print();
 	QP.setOptions(opt);
 
 	/* Calculate b */
 	double b[NCON];
-	calculate_b(x0, r0, b);
-
-#ifdef USE_SPARSE_MATRICES
-	/* create sparse matrices */
-	qpOASES::SymSparseMat *Hsp = new qpOASES::SymSparseMat(H_NROWS, H_NCOLS, H_NCOLS, H);
-	qpOASES::SparseMatrix *Asp = new qpOASES::SymSparseMat(A_NROWS, A_NCOLS, A_NCOLS, A);
-
-	/* Init QP - sparse */
-	int exitFlag = QP.init(Hsp, G, Asp, NULL, NULL, NULL, b, nWSR, &cpuTime);
-#else
-	/* Init QP - dense */
-	int exitFlag = QP.init(H, G, A, NULL, NULL, NULL, b, nWSR, &cpuTime);
-#endif
-
-	printf("exitFlag %i, cpu time %fs, nWSR = %i\n", exitFlag, cpuTime, nWSR);
-
-	return exitFlag;
-}
-
-int computeMPC(qpOASES::QProblem& QP, MPCPacketParams_t& params, MPCPacketResult_t& res)
-{
-	/* Calculate b */
-	double b[NCON];
 	calculate_b(params.x, params.r, b);
 
-	/* Compute hotstarted QP */
 	res.nWSR = 1000;
 	res.tExec = 10;
-	res.exitFlag = QP.hotstart(G, NULL, NULL, NULL, b, res.nWSR, &res.tExec);
+	res.exitFlag = QP.init(Hsp, G, Asp, NULL, NULL, NULL, b, res.nWSR, &res.tExec, initialPrimalSolution, 0, 0, 0, 0);
 
 	/* Get result */
 	QPout_t QPout;
