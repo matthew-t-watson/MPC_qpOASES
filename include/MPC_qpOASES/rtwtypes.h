351a9d363592fa237d5a5a0cadec9a7e7e6f4e22
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-29 17:38:21 +0100

    .

--- include/MPC_qpOASES/rtwtypes.h
+++ include/MPC_qpOASES/rtwtypes.h
@@ -5,7 +5,7 @@
  * File: rtwtypes.h
  *
  * MATLAB Coder version            : 3.3
- * C/C++ source code generated on  : 29-Mar-2018 17:32:27
+ * C/C++ source code generated on  : 29-Mar-2018 17:37:27
  */
 
 #ifndef RTWTYPES_H
