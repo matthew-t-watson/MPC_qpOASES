098ea11e7d706a6a2c536609e4ee3ecba899b0e2
Matthew Watson <matthewt_watson@hotmail.com>
2018-03-29 10:00:08 +0100

    .

--- include/MPC_qpOASES/MPC_defines.hpp
+++ include/MPC_qpOASES/MPC_defines.hpp
@@ -6,10 +6,10 @@
 #define NC		(5)
 #define NS		(12)
 #define NR		(5)
-#define NCON		(644)
+#define NCON		(374)
 #define H_NROWS		(36)
 #define H_NCOLS		(36)
-#define A_NROWS		(644)
+#define A_NROWS		(374)
 #define A_NCOLS		(36)
 
 #endif
--- include/MPC_qpOASES/calculate_b_types.h
+++ include/MPC_qpOASES/calculate_b_types.h
@@ -5,7 +5,7 @@
  * File: calculate_b_types.h
  *
  * MATLAB Coder version            : 3.3
- * C/C++ source code generated on  : 29-Mar-2018 09:55:08
+ * C/C++ source code generated on  : 29-Mar-2018 09:59:53
  */
 
 #ifndef CALCULATE_B_TYPES_H
