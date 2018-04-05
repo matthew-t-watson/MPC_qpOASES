de5383685985d9bf349c5112782a9346bff69e3c
Matthew Watson <matthewt_watson@hotmail.com>
2018-04-05 17:01:54 +0100

    .

--- include/MPC_qpOASES/calculate_u_types.h
+++ include/MPC_qpOASES/calculate_u_types.h
@@ -5,7 +5,7 @@
  * File: calculate_u_types.h
  *
  * MATLAB Coder version            : 3.3
- * C/C++ source code generated on  : 05-Apr-2018 15:32:28
+ * C/C++ source code generated on  : 05-Apr-2018 17:01:24
  */
 
 #ifndef CALCULATE_U_TYPES_H
--- src/calculate_u.c
+++ src/calculate_u.c
@@ -5,7 +5,7 @@
  * File: calculate_u.c
  *
  * MATLAB Coder version            : 3.3
- * C/C++ source code generated on  : 05-Apr-2018 15:32:28
+ * C/C++ source code generated on  : 05-Apr-2018 17:01:24
  */
 
 /* Include Files */
