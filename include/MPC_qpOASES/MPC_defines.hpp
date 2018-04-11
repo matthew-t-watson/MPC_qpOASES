f3bae5d7f213f987ebd89196c33665b386ba5eb6
Matthew Watson <matthewt_watson@hotmail.com>
2018-04-11 10:25:17 +0100

    .

--- include/MPC_qpOASES/MPC_defines.hpp
+++ include/MPC_qpOASES/MPC_defines.hpp
@@ -6,10 +6,10 @@
 #define NC		(2)
 #define NS		(8)
 #define NR		(2)
-#define NCON		(284)
+#define NCON		(286)
 #define H_NROWS		(20)
 #define H_NCOLS		(20)
-#define A_NROWS		(284)
+#define A_NROWS		(286)
 #define A_NCOLS		(20)
 
 #endif
--- include/MPC_qpOASES/calculate_u.h
+++ include/MPC_qpOASES/calculate_u.h
@@ -5,7 +5,7 @@
  * File: calculate_u.h
  *
  * MATLAB Coder version            : 3.3
- * C/C++ source code generated on  : 10-Apr-2018 17:20:52
+ * C/C++ source code generated on  : 10-Apr-2018 17:29:25
  */
 
 #ifndef CALCULATE_U_H
