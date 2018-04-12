aa124f43a847a4f705d527346d4dd020943920d9
Matthew Watson <matthewt_watson@hotmail.com>
2018-04-12 09:31:01 +0100

    .

--- include/MPC_qpOASES/calculate_b_types.h
+++ include/MPC_qpOASES/calculate_b_types.h
@@ -5,7 +5,7 @@
  * File: calculate_b_types.h
  *
  * MATLAB Coder version            : 3.3
- * C/C++ source code generated on  : 11-Apr-2018 16:28:01
+ * C/C++ source code generated on  : 12-Apr-2018 09:29:57
  */
 
 #ifndef CALCULATE_B_TYPES_H
--- include/MPC_qpOASES/calculate_cinf_types.h
+++ include/MPC_qpOASES/calculate_cinf_types.h
@@ -5,7 +5,7 @@
  * File: calculate_cinf_types.h
  *
  * MATLAB Coder version            : 3.3
- * C/C++ source code generated on  : 11-Apr-2018 16:28:02
+ * C/C++ source code generated on  : 12-Apr-2018 09:30:02
  */
 
 #ifndef CALCULATE_CINF_TYPES_H
